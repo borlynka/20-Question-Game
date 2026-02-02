a0f913027f0b70537337212d9e4d7487b71c051b
borlynka <borlynka@gmail.com>
2026-02-02 16:39:26 -0600

    saveQuestions method

--- QuestionsGame.java
+++ QuestionsGame.java
@@ -3,11 +3,15 @@
 // You should delete this comment and replace it with your class
 // header comment.
 
+import java.io.*;
+
+
 public class QuestionsGame {
+    QuestionNode game;
     // Your code here
 
     private static class QuestionNode {
-        public String data;
+        public final String data;
         public QuestionNode left;
         public QuestionNode right;
 
@@ -22,5 +26,47 @@ public class QuestionsGame {
             this.left = left;
             this.right = right;
         }
+    }
+
+    public QuestionsGame(String object){
+        QuestionNode game = new QuestionNode(object);
+
+    }
+
+
+
+
+
+
+    public void saveQuestions(PrintStream output){  
+
+        if(output== null){
+            throw new IllegalArgumentException();
+        }
+
+        questiontree(output, game);
+
+
+    }
+     
+    private void questiontree(PrintStream output,QuestionNode root){
+        if(root == null){
+            return;
+        }
+        if(root.left == null && root.right ==null){
+            output.println("A");
+            output.println(root.data);
+        }else{
+            output.println("Q");
+            output.println(root.data);
+        }
+        
+        if(root.left != null){
+            questiontree(output,root.left);
+        }
+        if(root.right != null){
+            questiontree(output,root.right);
+        }
+
     }
 }
